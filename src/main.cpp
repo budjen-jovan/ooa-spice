b4fb126940a85e95f9442c0690dd6a8e298d6fe2
Budjen Jovan <bj210608d@student.etf.bg.ac.rs>
2024-07-15 02:13:58 +0200

    -DN +Fdiff

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,19 @@
 
 int main() {
   // Matrix A({{2, 1, -1}, {-3, -1, 2}, {-2, 1, 2}});
-  Matrix A({{1, 2, 3}, {4, 5, 6}, {7, 8, 10}});
+  // Matrix A({{1, 2, 3}, {4, 5, 6}, {7, 8, 10}});
+  Matrix A({{1, 1, 1}, {2, 0, -1}, {0, 3, 1}});
 
-  Vector b({8, -11, -3});
+  // Vector b({8, -11, -3});
+  Vector b({5, 4, 2});
+
+  std::cout << "Matrix A:" << std::endl;
+  A.print();
+  std::cout << "Vector b:" << std::endl;
+  b.print();
+
+  std::cout << "Solving the system of linear equations Ax = b..." << std::endl;
+  std::cout << "\n" << std::endl;
 
   LinearSolver solver;
   try {
